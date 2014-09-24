179b206135a36c0c0a049e5a5e63978bfc1e189e
Roy Fu <fu90@purdue.edu>
2014-09-24 00:38:50 -0400

    debugging mobile

--- main.js
+++ main.js
@@ -79,6 +79,9 @@ function update() {
 function updateHtml() {
 	var html = '';
 	switch(page) {
+		// case landing:
+		// 	html += '';
+
 		case coder:
 			html += '<p>';
 			html += 'I didn&#39t grow up thinking that I would become a software developer. Like many others in my first grade class, I too, aspired to become Batman. I started to dwell in my basement, stay up late into the night, and wear an obscene amount of black. But after puberty hit, I came to the sad realization that beating criminals to a pulp just wasn&#39t what I was cut out for. But I couldn&#39t let all these bat-like qualities go to waste, so I made most logical decision I could and became a programmer. Anyways, here are my projects:';
