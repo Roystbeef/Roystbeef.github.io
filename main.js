2e0779dd20448943d3e0ff708c7a7db75e84a95c
Roy Fu <fu90@purdue.edu>
2014-09-24 23:49:55 -0400

    Added some project views but they look shitty

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var page = 0;
+var page = 1;
 var landing = 0;
 var coder = 1;
 var designer = 2;
@@ -86,6 +86,30 @@ function updateHtml() {
 			html += '<p>';
 			html += 'I didn&#39t grow up thinking that I would become a software developer. Like many others in my first grade class, I too, aspired to become Batman. I started to dwell in my basement, stay up late into the night, and wear an obscene amount of black. But after puberty hit, I came to the sad realization that beating criminals to a pulp just wasn&#39t what I was cut out for. But I couldn&#39t let all these bat-like qualities go to waste, so I made most logical decision I could and became a programmer. Anyways, here are my projects:';
 			html += '</p>';
+			html += '<h2 class="break"><span>PROJECTS</span></h2>';
+			html += "<h3 class='projectTitle'><a href='http://amishhero.com' target='_blank'>AMISH HARVEST HERO</a></h3>";
+			html += "<p class='projectContent'>Web Game</p>";
+			html += "<img class='projectPic' src='Assets/Images/AmishHero.png'></img>";
+			html += "<h3 class='projectTitle'><a href='http://palette.town' target='_blank'>PALETTE TOWN</a></h3>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<h3 class='projectTitle'><a href='http://ytwall.com' target='_blank'>YT WALL</a></h3>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+			html += "<p class='projectContent'>Web Application</p>";
+
 			break;
 		case designer:
 			html += '<p>Coming Soon!</p>';
