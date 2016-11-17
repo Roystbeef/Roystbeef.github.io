e3adcbc7903c0b304e08bc1afba8a0d81eef0d6b
Roy Fu <royfu@purdue.edu>
2016-11-17 17:30:44 -0500

    Add bill cipher effects

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@ var song = new Audio('gravityfalls.wav');
 var bill = document.getElementById('bill');
 bill.addEventListener('click', function() {
   if (song.paused) {
+    bill.style.opacity = '1';
     song.play();
   } else {
+    bill.style.opacity = '0';
     song.pause();
   }
 });
+
+bill.addEventListener('mouseenter', function() {
+  bill.style.opacity = '1';
+})
+
+bill.addEventListener('mouseleave', function() {
+  if (song.paused) {
+    bill.style.opacity = '0';
+  }
+})
